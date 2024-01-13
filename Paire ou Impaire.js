a578ab859594cd58e1e7cc95e6a1f024a5d4f2ef
Lemecdeparla <lemontoisdu33@gmail.com>
2024-01-13 20:36:32 +0100

    paire ou impaire Terminé

---
+++ Paire ou Impaire.js
@@ -0,0 +1,26 @@
+let entree = process.argv[2]
+function paireounon(nbr) {
+    if (nbr % 2 == 0)
+    {
+        return "paire"
+    }
+    else
+    {
+        return "impaire"
+    }
+}
+
+if (isNaN(entree))
+{
+    console.log("Tu ne me la mettras pas à l’envers.");
+}
+else if(entree < 0)
+{
+    entree = Math.abs(entree)
+    console.log(paireounon(entree))
+}
+else
+{
+    console.log(paireounon(entree))
+}
+
