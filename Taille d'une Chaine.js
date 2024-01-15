fd237ec9722e98203584246d18ab59aedbfccd36
Lemecdeparla <lemontoisdu33@gmail.com>
2024-01-15 01:50:59 +0100

    Taille d'une chaine refactoring

--- Taille d'une Chaine.js
+++ Taille d'une Chaine.js
@@ -1,21 +1,12 @@
 /* récupérations des arguments d'entrée et supression du chemin d'accès et du nom du fichier */
-let entree = process.argv
-for (i = 0; i < 2; i ++){
-    entree.shift()
-}
-/*vérification du nombre d'argument en entrée*/
-let lengthentree = 0 
-for (i = 0; entree[i] !== undefined; i ++ ){
-    lengthentree += 1
-}
-let str = entree[0]
+let entree = process.argv[2]
 /* erreur si il n'y a pas ou plus d'un argument ou si l'entrée est un nombre */
-if ( !isNaN(str) || lengthentree > 1 || lengthentree == 0) {
+if ( !isNaN(entree) || process.argv[2] == undefined || process.argv[3] != undefined) {
     console.log('erreur')
 }
 /* split des charachtères de la chaine pour les incrémenter dans le tableau char */
 else {
-    let char = str.split('')
+    let char = entree.split('')
     /* comptage et des éléments dans le tableau char */
     let length = 0
     for (i = 0; char[i] !== undefined; i ++){
