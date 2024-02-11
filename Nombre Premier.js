4cb5232fb26c633ac4e29d5b27b87698f7dd60a6
Lemecdeparla <lemontoisdu33@gmail.com>
2024-02-11 17:24:42 +0100

    Nombre Enteir Refactoring

--- Nombre Premier.js
+++ Nombre Premier.js
@@ -1,6 +1,6 @@
 let nbr = Number(process.argv[2])
 
-/* erification que l'entrée est bien un nombre */
+/* verification que l'entrée est bien un nombre et qu'il n'est pas égale a 1 */
 if (isNaN(nbr) || nbr < 2){
     console.log("erreur")
 }else{
@@ -8,11 +8,11 @@ if (isNaN(nbr) || nbr < 2){
     let resultat = ""
     /* boucle permettant de savoir si nbr est divisible par autre chose que 1 ou lui même */
     for (let i = 2; i < nbr; i ++){
-        if (nbr % i == 0 & nbr !== 2){
+        if (nbr % i == 0 & 2 <= nbr){
             resultat = "non"
         }
     }
-    /* affichage du résultat en fonction du de l'état de résultat */
+    /* affichage du résultat en fonction de l'état de résultat */
     if (resultat == "non"){
         console.log(nbr," n'est pas un nombre premier")
     }else{
